a8d6577acbbec9c88c2f4d098aa1c2d9499a32d3
ahmed <ahmed.elkhabbazi@students.hs-mainz.de>
2026-06-24 00:56:28 +0200

    add pagination, remove note, restrict praxis creation to admin

--- src/main/java/de/itsec/api/data/dto/request/TerminBookingRequestDto.java
+++
@@ -1,4 +0,0 @@
-package de.itsec.api.data.dto.request;
-
-/** Optional payload when booking a slot. {@code note} is stored encrypted at rest. */
-public record TerminBookingRequestDto(String note) {}
