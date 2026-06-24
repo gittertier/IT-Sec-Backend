64e1478d64eb80f1eb7862567234836e22bb1aed
gittertier <140616399+gittertier@users.noreply.github.com>
2026-06-25 00:02:10 +0200

    make address optional

--- src/main/java/de/itsec/api/data/dto/response/AddressDto.java
+++ src/main/java/de/itsec/api/data/dto/response/AddressDto.java
@@ -5,6 +5,9 @@ import de.itsec.api.data.Address;
 public record AddressDto(String street, String houseNumber, String postalCode, String city) {
 
   public static AddressDto from(Address address) {
+    if (address == null) {
+      return new AddressDto(null, null, null, null);
+    }
     return new AddressDto(
         address.getStreet(), address.getHouseNumber(), address.getAreaCode(), address.getCity());
   }
